124698c447aa9bd61bb0d338e5b2cca7e9f96f2c
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-11-23 14:40:59 +0700

    Update name7.go

--- name7.go
+++ name7.go
@@ -17,4 +17,7 @@ func main() {
 	startB := time.Now()
 	var b strings.Builder
 	for i := 0; i < 10000; i++ {
-	    b.WriteString("b")
+		b.WriteString("b")
+	}
+	fmt.Println("b", time.Since(startB))
+}
