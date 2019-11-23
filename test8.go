99434aae9ea9b981da1f77c290c6c55775cca61f
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-11-23 12:50:46 +0700

    Update test8.go

--- test8.go
+++ test8.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(strings.Replace("Hello World", "1", "x" 2))
-	fmt.Println(strings.Replace("Hello World", "1" , "x" ,-1))
+	fmt.Println(strings.Replace("Hello World", "1", "x", 2))
+	fmt.Println(strings.Replace("Hello World", "1", "x", -1))
 }
