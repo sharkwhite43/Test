183c04231e62d5ae49749d9412c0750d060d6568
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-11-23 14:25:08 +0700

    Update name6.go

--- name6.go
+++ name6.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var b strings.Builder
 	b.WriteString("Hello")
-	b.WriteString("")
+	b.WriteString(" ")
 	b.WriteString("World")
-	fmt.Println(b.string())
+	fmt.Println(b.String())
 }
