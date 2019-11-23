483d6935c32f6d0756a8a03d8d2e9795efd4c959
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-11-23 12:19:21 +0700

    Update test5.go

--- test5.go
+++ test5.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(strings.HasSuffix("Hello World", "world"))
-	fmt.Println(strings.HasSuffix("Hello World", "World"))
+	fmt.Println(strings.HasSuffix("Hello World", "hello"))
+	fmt.Println(strings.HasSuffix("Hello World", "Hello"))
 }
