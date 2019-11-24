c55c11f2e7d6c18efabf53c251d1dbfda8cd86d9
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-11-24 12:37:07 +0700

    Update test21.go

--- test21.go
+++ test21.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	n, e := fmt.Print("Hello", "World", 123, 456, "\n")
-	fmt.Print("number of bytes written :=", n, "\n")
+	fmt.Print("number of bytes written :", n, "\n")
+	fmt.Print("write error encounteredb:", e, "\n")
 }
