40ad60d9011a91fc02bb387c03351f18c65b8e3a
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-04 09:20:50 +0700

    Update test26.go

--- week4/test26.go
+++ week4/test26.go
@@ -1,9 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	student_1 := "Goku"
 	student_2 := "Gohan"
 	student_3 := "Vegeta"
 	student_4 := "piccolo"
+
+	fmt.Println(student_1, student_2, student_3, student_3)
 
 }
