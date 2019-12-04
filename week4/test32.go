1df5f8a42ce21bc566d8db9f9c2f41e92ac26072
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-04 21:52:43 +0700

    Update test32.go

--- week4/test32.go
+++ week4/test32.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	number := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(number[1])
-	number [1] := 15
-	fmt.Println(number [1])
+	numbers := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers[1])
+	numbers[1] = 15
+	fmt.Println(numbers[1])
+	length := len(numbers)
 
 }
