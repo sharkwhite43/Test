9408634cc4c789a039465211b9cfa0d332096e69
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-04 21:44:04 +0700

    Update test30.go

--- week4/test30.go
+++ week4/test30.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func main()  {
-	primes := [6] int {1, 3 , 4 , 5 , 8 , 9}
+func main() {
+	primes := [6]int{1, 3, 4, 5, 8, 9}
 	fmt.Println(primes[4])
-	fmt.Println([2])
-	
+	fmt.Println(primes[2])
+
 }
