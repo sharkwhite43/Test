489cc0e620a67af29921fc882341f49a44a551d7
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-07 21:24:40 +0700

    Update test42.go

--- week4/test42.go
+++ week4/test42.go
@@ -1,9 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	a := []string{"A", "B", "C", "D", "E"}
-	deleteIndex := 2
-	a = append(a[:deleteIndex], a[deleteIndex+1:]...}
+	deleteIndex := 3
+	a = append(a[:deleteIndex], a[deleteIndex+1:]...)
 	fmt.Println(a)
 
 }
