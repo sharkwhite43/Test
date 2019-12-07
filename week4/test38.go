970fb54bef284bf24c56874b82ccbf938e52e80f
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-07 19:13:30 +0700

    Update test38.go

--- week4/test38.go
+++ week4/test38.go
@@ -6,7 +6,7 @@ func main() {
 	alphabets := [4]string{"A", "B", "C", "D"}
 	x := alphabets[:]
 	y := alphabets[:3]
-	z := alphabets[1:]
+	z := alphabets[2:]
 	fmt.Println(x)
 	fmt.Println(y)
 	fmt.Println(z)
