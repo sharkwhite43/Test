4c4a4d4577ffc83ec1ed72919e902b8929ecfe8a
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-08 13:44:33 +0700

    Update test47.go

--- week4/test47.go
+++ week4/test47.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	elements := make(map[string]map[string]string)
 	elements["H"] = map[string]string{"name": "Hydrogen", "state": "gas"}
+	elements["He"] = map[string]string{"name": "Helium", "state": "gas"}
 
 }
