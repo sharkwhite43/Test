d82b0f41a402da820262565b3e03d6ae0e6fa7ee
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-08 13:44:56 +0700

    Update test47.go

--- week4/test47.go
+++ week4/test47.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	elements := make(map[string]map[string]string)
 	elements["H"] = map[string]string{"name": "Hydrogen", "state": "gas"}
 	elements["He"] = map[string]string{"name": "Helium", "state": "gas"}
+	fmt.Println(elements)
 
 }
