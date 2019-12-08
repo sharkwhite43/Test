73997f6510926ce87702005d213d6ccc88daf073
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-08 13:55:50 +0700

    Update test49.go

--- week4/test49.go
+++ week4/test49.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	elements := make(map[string]string)
 	elements["H"] = "Hydrogen"
 	elements["He"] = "Helium"
 	elements["Li"] = "Lithium"
+	fmt.Println(elements)
+
+	delete(elements, "h")
 
 }
