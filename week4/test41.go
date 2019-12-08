4be83f814695c5385a65e5dc53852fc1acd7ed5c
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-08 23:52:08 +0700

    Update test41.go

--- week4/test41.go
+++ week4/test41.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	humes := []string{"Tan", "Nan", "Ter"}
+	humans := []string{"Tan", "Nan", "Ter"}
 	names := []string{"Baot", "Aum", "meena"}
-	names = append(names, humes...)
+	names = append(names, humans...)
 	fmt.Println(names)
 
 }
