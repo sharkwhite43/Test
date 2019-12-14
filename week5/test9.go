2c07540a405502bdfecbb0390eca85701331746a
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-14 18:23:33 +0700

    Update test9.go

--- week5/test9.go
+++ week5/test9.go
@@ -7,13 +7,15 @@ func main() {
 	switch number {
 	case 0:
 		fmt.Println("Zero")
-	}
-    case 1:
-	    fmt.Println("one")
-    case 2:
+	case 1:
+		fmt.Println("one")
+	case 2:
 		fmt.Println("two")
 	case 3:
 		fmt.Println("tree")
 	case 4:
-	    fmt.Println("four")
+		fmt.Println("four")
+	default:
+		fmt.Println("unknow")
+	}
 }
