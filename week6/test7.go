fe87205f5c1daf97fdb32046f628370e4681bd10
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-14 20:24:04 +0700

    Update test7.go

--- week6/test7.go
+++ week6/test7.go
@@ -1,14 +1,8 @@
 package main
 
-import "fmt"
+func say(greet string) func(string) string {
 
-func main() {
-	for i := 1; i <= 10; i = i + 1 {
-		if i%2 == 0 {
-			fmt.Println(i, "even")
-		} else {
-			fmt.Println(i, "odd")
-		}
+	return func(name string) string {
 
 	}
 }
