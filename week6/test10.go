89946b776d571cc86e95ea88afc04b70a298433f
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-14 21:18:37 +0700

    Update test10.go

--- week6/test10.go
+++ week6/test10.go
@@ -2,16 +2,13 @@ package main
 
 import "fmt"
 
-func suntract(number1, number2 int) {
-	number1 = number1 - 1
-	number2 = number2 - 1
-
+func suntract(number int) {
+	number = number - 1
 }
 
 func main() {
 	x := 10
-	y := 15
-	suntract(x, y)
-	fmt.Println(x, y)
+	suntract(x)
+	fmt.Println(x)
 
 }
