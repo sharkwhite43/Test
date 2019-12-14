72e4f805eb4fff8b542cbb7c61d638044573effc
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-14 18:17:07 +0700

    Update test8.go

--- week5/test8.go
+++ week5/test8.go
@@ -6,11 +6,11 @@ func main() {
 	score := 75
 	if score > 80 {
 		fmt.Println("A")
-	}else if score > 70 {
+	} else if score > 70 {
 		fmt.Println("B")
-	}else if score > 60 {
+	} else if score > 60 {
 		fmt.Println("C")
-	}else if score > 50 {
-		
-
+	} else if score > 50 {
+		fmt.Println("D")
+	}
 }
