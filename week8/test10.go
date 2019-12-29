13623a25f30f1db9228bf6e0be7dac7bef99066b
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-29 22:16:59 +0700

    Update test10.go

--- week8/test10.go
+++ week8/test10.go
@@ -5,11 +5,11 @@ import "fmt"
 type I interface{}
 
 func desc(i I) {
-	fmt.Println("%v , %T \n", i, i)
+	fmt.Printf("%v , %T \n", i, i)
 }
 
 func main() {
-	var i T
+	var i I
 	i = 3.1459265359
 	desc(i)
 	i = "Hello World"
