ddfcd49fe3fb1227df3d529c2ca40e7939809fb4
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-29 11:42:51 +0700

    Update test2.go

--- week8/test2.go
+++ week8/test2.go
@@ -10,7 +10,7 @@ type student struct {
 
 func main() {
 	var std [10]student
-	std[0] = student{"Tan", 19, "Tawan"}
+	std[0] = student{"Tan", 19, "Tantawanais@gmail.com"}
 
 	fmt.Println(std[0])
 	fmt.Println(std[0].name)
