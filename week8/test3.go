78ae82e0b60c7c6a95d95fe9d95bf37660ee5a61
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2019-12-29 11:52:27 +0700

    Update test3.go

--- week8/test3.go
+++ week8/test3.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type student struct {
 	name  string
 	age   int
@@ -13,6 +15,11 @@ func main() {
 	a.email = "TanTawan23@gmail.com"
 
 	b := student{"Tle", 19, "Tle@mail.com"}
+
 	c := student{name: "Ter", email: "Ter@mail.com"}
+
+	d := student{age: 22}
+
+	fmt.Println(a)
 
 }
