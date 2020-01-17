4b27ce96d1f45c70ef063fd5eb683203a058ca70
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-01-17 19:48:35 +0700

    up

--- week9/test1.go
+++ week9/test1.go
@@ -1,9 +1,10 @@
-package main
+package myMath
 
-func Average(numbeer ...float64) float64 {
+func Average(numbers ...float64) float64 {
 	var total float64
 	for _, v := range numbers {
-
+		total = total + v
 	}
+	return total / float64(len(numbers))
 
 }
