e47e9b107450874a5db3ff4886685e257bda0328
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-02-16 20:27:58 +0700

    Update test1.go

--- week11/test1.go
+++ week11/test1.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 )
 
 func main() {
-	reader := string.NewReader("HelloWorld")
+	reader := strings.NewReader("Hello World")
 	p := make([]byte, 3)
 	for {
 		n, err := reader.Read(p)
 		if err == io.EOF {
 			break
 		}
+		fmt.Println(string(p[:n]))
 	}
-	fmt.Println(string(p[:n]))
 
 }
