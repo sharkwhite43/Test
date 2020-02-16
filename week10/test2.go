f73aa5a77de8c03c74af9685a54233dfe456413b
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-02-16 19:48:30 +0700

    Update test2.go

--- week10/test2.go
+++ week10/test2.go
@@ -1,12 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func say(txt string) {
 	for i := 0; i < 3; i++ {
-		fmt.Println(time.Now() , ": , i ,":" , txt)
+		fmt.Println(time.Now(), ":", i, ":", txt)
+		time.Sleep(time.Millisecond)
 
 	}
-
-}
-func main() {
 
 }
