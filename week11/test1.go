8fc7357225b0f1e64abc589a5282af85fb35adfc
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-02-16 20:23:30 +0700

    Update test1.go

--- week11/test1.go
+++ week11/test1.go
@@ -1,10 +1,15 @@
 package main
 
+import "io"
+
 func main() {
 	reader := string.NewReader("HelloWorld")
 	p := make([]byte, 3)
 	for {
 		n, err := reader.Read(p)
-		
+		if err == io.EOF {
+			break
+		}
+	}
 
 }
