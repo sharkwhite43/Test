4ef1336690f6a7e4828349bca7214e724c328438
sharkwhite43 <57710017+sharkwhite43@users.noreply.github.com>
2020-02-23 21:38:40 +0700

    Update work.go

--- Codetest/work.go
+++ Codetest/work.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func getDrives() (r []string) {
 	fmt.Println("input : ")
 	var get string
 	fmt.Scan(&get)
+	f, err := os.Open(string(get) + ":\\")
+	if err == nil {
+
+	}
+	return
 
 }
